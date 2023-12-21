bf64873f948c3f857e9e231e0d735988cb530ee8
Mattias <106891644+Neniuk@users.noreply.github.com>
2023-12-21 13:03:39 +0200

    Update index.tsx

--- src/components/weatherCard/index.tsx
+++ src/components/weatherCard/index.tsx
@@ -1,1 +1,15 @@
-export {};
+import React, { Component } from "react";
+import "./styles.css";
+
+class WeatherCard extends Component {
+	render() {
+		return (
+			<div className="weather-card">
+				<div className="weather-card-left-column"></div>
+				<div className="weather-card-right-column"></div>
+			</div>
+		);
+	}
+}
+
+export default WeatherCard;
