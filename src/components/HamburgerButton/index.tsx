aac2c165a51bc4c156480210b23cd5c09112587d
Mattias <106891644+Neniuk@users.noreply.github.com>
2023-12-21 18:09:08 +0200

    added hamburger button component

---
+++ src/components/HamburgerButton/index.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+type HamburgerButtonProps = {};
+
+class HamburgerButton extends Component<HamburgerButtonProps> {
+    render() {
+        return (
+            <div>
+                <div></div>
+            </div>
+        );
+    }
+}
+
+export default HamburgerButton;
