0774690f3cb254e2e146d8724342a86adc342286
Mattias <106891644+Neniuk@users.noreply.github.com>
2023-12-21 17:51:01 +0200

    simplified weather component

--- src/components/WeatherCard/index.tsx
+++ src/components/WeatherCard/index.tsx
@@ -13,24 +13,14 @@ class WeatherCard extends Component<WeatherCardProps> {
         const { location } = this.props;
         return (
             <div className="mx-auto mt-4 flex max-w-95% flex-row justify-between rounded-2xl bg-sky-300 p-4 text-white sm:max-w-96">
-                <div className="">
-                    <div className="text-left">
-                        <h1 className="text-3xl">Lahti</h1>
-                    </div>
-                    <div className="text-sky-100">
-                        <h2>Partly Cloudy</h2>
-                    </div>
-                    <div className="text-sky-100">
-                        <h2>-12°C / -6°C</h2>
-                    </div>
+                <div className="text-left">
+                    <h1 className="text-3xl">Lahti</h1>
+                    <h2 className="text-sky-100">Partly Cloudy</h2>
+                    <h2 className="text-sky-100">-12°C / -6°C</h2>
                 </div>
                 <div className="text-right">
-                    <div className="">
-                        <h1 className="text-4xl">-11°C</h1>
-                    </div>
-                    <div className="">
-                        <h2>Wind speed: 3.7 m/s</h2>
-                    </div>
+                    <h1 className="text-4xl">-11°C</h1>
+                    <h2 className="text-sky-100">Wind speed: 3.7 m/s</h2>
                 </div>
             </div>
         );
