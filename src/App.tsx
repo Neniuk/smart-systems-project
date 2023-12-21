b1c1a3a322ce4ab79e9cbc668d6ec37f99bd49ba
Mattias <106891644+Neniuk@users.noreply.github.com>
2023-12-21 13:54:23 +0200

    tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,14 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import WeatherCard from "./components/WeatherCard";
+
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<WeatherCard />
+		</div>
+	);
 }
 
 export default App;
--- src/components/weatherCard/index.tsx
+++ src/components/weatherCard/index.tsx
@@ -1,12 +1,42 @@
 import React, { Component } from "react";
 import "./styles.css";
 
+type LocationWeather = {
+	location: string;
+	currentTemperature: number;
+	minimumTemperature: number;
+	maximumTemperature: number;
+	humidity: number;
+	windSpeed: number;
+	weather: string;
+	weatherDescription: string;
+	weatherIcon: string;
+};
+
 class WeatherCard extends Component {
 	render() {
 		return (
-			<div className="weather-card">
-				<div className="weather-card-left-column"></div>
-				<div className="weather-card-right-column"></div>
+			<div className="flex flex-row justify-between max-w-95% mx-auto mt-4 sm:max-w-96 bg-sky-300 rounded-2xl p-4 text-white">
+				<div className="weather-card-left-column">
+					<div className="weather-location">
+						<h1>City, Country</h1>
+					</div>
+					<div className="weather-description">
+						<h2>Weather description</h2>
+					</div>
+					<div className="weather-temperature-min-max">
+						<h2>Min: 0°C</h2>
+						<h2>Max: 0°C</h2>
+					</div>
+				</div>
+				<div className="weather-card-right-column">
+					<div className="weather-temperature">
+						<h1>0°C</h1>
+					</div>
+					<div className="weather-wind-speed">
+						<h2>Wind speed: 0 m/s</h2>
+					</div>
+				</div>
 			</div>
 		);
 	}
