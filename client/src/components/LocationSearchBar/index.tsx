8e98ed4722f73e1953db037e842a099c3bd6ce06
Anh Vainionpaa <anh.vainionpaa@gmail.com>
2024-01-02 23:17:02 +0200

    update HamburgerButton

--- client/src/components/LocationSearchBar/index.tsx
+++ client/src/components/LocationSearchBar/index.tsx
@@ -1,6 +1,6 @@
 // LocationSearchBarCard.tsx
 import React, { useState } from 'react';
-import './LocationSearchBar.css'; // Ensure this is the correct path to your CSS file
+import './LocationSearchBar.css'; 
 
 const LocationSearchBar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -16,23 +16,40 @@ const LocationSearchBar: React.FC = () => {
         <div className="bar2"></div>
         <div className="bar3"></div>
       </div>
+
       <div className={`menu-content ${isOpen ? 'menu-open' : 'menu-closed'}`}>
+      <div className="title-container">
+      <h1 className="weather-planner-title">
+          <span className="arrow-icon">&#8592;</span> {/* This is the arrow icon */}
+          Weather Planner
+        </h1>
+        </div>
+
+        <div className="current-location">
+          <span className="current-location-text">Current location:</span>
+          <span className="current-locationLahti">Lahti</span>
+        </div>
+        
         <div className="search-section">
           <input type="text" placeholder="search location" />
         </div>
-        <div className="current-location">
-          <span className="current-location-text">Current location</span>
-          <div className="current-locationLahti">Lahti</div>
-        </div>
+        
         <div className="recent-locations">
-        <span className="recent-locations-text">Recent locations</span>
+        <span className="recent-locations-text">Recently searched:</span>
           <div className="location">Vantaa</div>
           <div className="location">Oulu</div>
           <div className="location">Lappeenranta</div>
           <div className="location">Hämeenlinna</div>
           <div className="location">Helsinki</div>
         </div>
-        <button className="manage-locations">Manage locations</button>
+
+        <div className="menu-options">
+          <div className="menu-options">ACTIVITIES</div>
+          <div className="menu-options">SETTINGS</div>
+          <div className="menu-options">SETTINGS</div>
+          <div className="menu-options">HOME</div>
+        </div>
+        
       </div>
     </div>
   );
