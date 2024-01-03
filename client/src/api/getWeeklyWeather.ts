43172d03d155461d05eca696c1288a3c48e83e79
Mattias <106891644+Neniuk@users.noreply.github.com>
2024-01-03 17:36:24 +0200

    Create getWeeklyWeather.ts

---
+++ client/src/api/getWeeklyWeather.ts
@@ -0,0 +1,30 @@
+import Location from "../models/Location";
+
+const ADDRESS = "http://localhost:";
+// Reference: https://stackoverflow.com/questions/49579028/adding-an-env-file-to-a-react-project
+const PORT = process.env.REACT_APP_SERVER_PORT || "5000";
+
+const getWeeklyWeather = async (location: Location) => {
+    // console.log("getWeather() called");
+    const req: string = ADDRESS + PORT + "/weather";
+
+    try {
+        const res: Response = await fetch(req, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ location: location }),
+        });
+        // console.log("Response: ", res);
+
+        const data: object = await res.json();
+        // console.log("Data: ", data);
+
+        return data;
+    } catch (err: any) {
+        console.log("Error: ", err);
+    }
+};
+
+export default getWeeklyWeather;
