002aa486ab24e3c59b4a1383bfd0e926d424a89f
Mattias <106891644+Neniuk@users.noreply.github.com>
2024-01-03 17:29:47 +0200

    Removed nested links

--- client/src/components/ExploreCard/index.tsx
+++ client/src/components/ExploreCard/index.tsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 type ExploreCardProps = {};
 
@@ -7,20 +6,17 @@ class ExploreCard extends Component<ExploreCardProps> {
     render() {
         return (
             // TODO: Adjust card background gradient to match the figma design
-            // TODO: Make the div clickable and route to activities page
-            <Link to="/activities">
-                <div className="min-w-95% sm:min-w-128 sm:max-w-128 max-w-95% shadow-outer from-mainCardPrimary via-mainCardPrimary to-mainCardSecondary flex flex-row justify-between rounded-2xl bg-gradient-to-l p-4">
-                    <div>
-                        <img src="" alt="Location Marker Icon"></img>
-                    </div>
-                    <div className="text-right">
-                        <h1 className="text-3xl">Explore</h1>
-                        <h2 className="text-textLightSecondary">
-                            Find activities near you
-                        </h2>
-                    </div>
+            <div className="min-w-95% sm:min-w-128 sm:max-w-128 max-w-95% shadow-outer from-mainCardPrimary via-mainCardPrimary to-mainCardSecondary flex flex-row justify-between rounded-2xl bg-gradient-to-l p-4">
+                <div>
+                    <img src="" alt="Location Marker Icon"></img>
                 </div>
-            </Link>
+                <div className="text-right">
+                    <h1 className="text-3xl">Explore</h1>
+                    <h2 className="text-textLightSecondary">
+                        Find activities near you
+                    </h2>
+                </div>
+            </div>
         );
     }
 }
