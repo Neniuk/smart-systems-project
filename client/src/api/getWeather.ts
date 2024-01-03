288ce971fb7c18476fef2443ce0a90841c79ad7e
Mattias <106891644+Neniuk@users.noreply.github.com>
2024-01-03 17:51:22 +0200

    client side environment variables not working yet

--- client/src/api/getWeather.ts
+++ client/src/api/getWeather.ts
@@ -4,6 +4,8 @@ const ADDRESS = "http://localhost:";
 // Reference: https://stackoverflow.com/questions/49579028/adding-an-env-file-to-a-react-project
 const PORT = process.env.REACT_APP_SERVER_PORT || "5000";
 
+console.log("SERVER PORT: ", PORT);
+
 const getWeather = async (location: Location) => {
     // console.log("getWeather() called");
     const req: string = ADDRESS + PORT + "/weather";
