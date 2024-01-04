968eff439b2fc73702507abe1e5440fc72f6d93b
Mattias <106891644+Neniuk@users.noreply.github.com>
2024-01-04 18:41:24 +0200

    Added weather fetching changes

--- client/src/pages/HomePage/index.tsx
+++ client/src/pages/HomePage/index.tsx
@@ -9,44 +9,23 @@ import getWeather from "../../api/getWeather";
 import Location from "../../models/Location";
 import BackgroundImage from "../../components/BackgroundImage";
 
-type HomePageProps = {};
+type HomePageProps = {
+    weather?: object;
+    location?: Location;
+};
 
-// TODO: Add props to component
-const HomePage = () => {
-    const [location, setLocation] = useState<Location | undefined>(undefined);
-    const [weatherData, setWeatherData] = useState<object | undefined>(
-        undefined
-    );
+// TODO: Add forecast prop and add forecast to App.tsx
+const HomePage = (props: HomePageProps) => {
+    console.log(props);
     const [selectedForecast, setSelectedForecast] = useState<string>("weekly");
-
-    useEffect(() => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(
-                (position) => {
-                    const location: Location = {
-                        longitude: position.coords.longitude,
-                        latitude: position.coords.latitude,
-                    };
-                    localStorage.setItem("location", JSON.stringify(location));
-                    setLocation(location);
-                    getWeather(location).then((data) => {
-                        setWeatherData(data);
-                    });
-                },
-                (error) => {
-                    console.error("Error retrieving geolocation data", error);
-                }
-            );
-        } else {
-            console.error("Geolocation is not supported by this browser");
-        }
-    }, []);
-
     return (
         <div className="from-backgroundPrimary to-backgroundSecondary min-h-screen bg-gradient-to-b">
             <div className="content text-textLight flex flex-col items-center justify-center gap-8 pt-16">
-                <LocationSearchBar /> {/* Corrected the component name here */}
-                <WeatherCard location={location} weatherData={weatherData} />
+                <LocationSearchBar />
+                <WeatherCard
+                    location={props.location}
+                    weatherData={props.weather}
+                />
                 <Link to="/activities">
                     <ExploreCard />
                 </Link>
