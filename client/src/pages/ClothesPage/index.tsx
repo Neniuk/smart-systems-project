610b069592a7428f1d2370e5f87d759829df7a72
JesseDong9527 <154630957+JesseDong9527@users.noreply.github.com>
2024-01-06 11:25:37 +0200

    Revert "Update index.tsx"
    This reverts commit db9ff018a29f55e73d3bb23b65421d9d1d312588.

--- client/src/pages/ClothesPage/index.tsx
+++ client/src/pages/ClothesPage/index.tsx
@@ -1,87 +1,81 @@
 import React, { useEffect, useState } from "react";
-import HatCard from "../../components/HatCard"
+import HatCard from "../../components/HatCard";
 import UpperBodyCard from "../../components/UpperBodyCard";
 import LowerBodyCard from "../../components/LowerBodyCard";
 import FootCard from "../../components/FootCard";
 import getWeather from "../../api/getWeather";
 import Location from "../../models/Location";
+import LocationSearchBar from "../../components/LocationSearchBar";
 type WeatherCard = {
     weatherData?: any;
 };
-const ClothesPage: React.FC = () => {
 
-    const [location, setLocation] = useState<Location | undefined>(undefined);
-    const [weatherData, setWeatherData] = useState<object | undefined>(
-        undefined
-    );
-    const [celsius, setCelsius] = useState<{
-        minTempCelsius: number,
-        maxTempCelsius: number,
-    } | null>(null);
+type ClothesPageProps = {
+    location?: Location;
+    weatherData?: object;
+};
 
+const ClothesPage = (props: ClothesPageProps) => {
+    // const [celsius, setCelsius] = useState<{
+    //     minTempCelsius: number;
+    //     maxTempCelsius: number;
+    // } | null>(null);
 
-    useEffect(() => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(
-                (position) => {
-                    const location: Location = {
-                        longitude: position.coords.longitude,
-                        latitude: position.coords.latitude,
-                        time: 0
-                    };
-                    localStorage.setItem("location", JSON.stringify(location));
-                    setLocation(location);
-                    getWeather(location).then((data) => {
-                        setWeatherData(data);
+    // setCelsius({
+    //     minTempCelsius: 0,
+    //     maxTempCelsius: 0,
+    // });
 
-                        const weatherData: any = data as unknown;
+    // if (!celsius)
+    //     return <div className=" flex h-screen w-screen items-center"></div>;
 
-                        const minTempKelvin: number = weatherData?.main.temp_min;
-                        const minTempCelsius: number = minTempKelvin - 273.15;
-
-                        const maxTempKelvin: number = weatherData?.main.temp_max;
-                        const maxTempCelsius: number = maxTempKelvin - 273.15;
-
-                        setCelsius({
-                            maxTempCelsius,
-                            minTempCelsius
-                        })
-                    });
-                },
-                (error) => {
-                    console.error("Error retrieving geolocation data", error);
-                }
-            );
-        } else {
-            console.error("Geolocation is not supported by this browser");
-        }
-    }, []);
-
-    if (!celsius) return <div className=" w-screen h-screen flex items-center"></div>;
-
-    return <>
-        <div className=" w-screen h-screen flex items-center">
-            <div className="w-4/5 mx-auto my-auto flex bg-white rounded-md  flex-wrap">
-                <div className="flex-1 flex flex-col">
-                    <div className="p-4">
-                        <HatCard maxTempCelsius={celsius.maxTempCelsius} minTempCelsius={celsius.minTempCelsius} />
-                    </div>
-                    <div className="flex-1 p-4">
-                        <UpperBodyCard maxTempCelsius={celsius.maxTempCelsius} minTempCelsius={celsius.minTempCelsius} />
-                    </div>
-                </div>
-                <div className="flex-1 flex flex-col">
-                    <div className="flex-4  p-4">
-                        <div className="flex-1 ">
-                            <LowerBodyCard maxTempCelsius={celsius.maxTempCelsius} minTempCelsius={celsius.minTempCelsius} />
+    return (
+        <>
+            <div className=" flex h-screen w-screen items-center">
+                <LocationSearchBar /> {/* Inserted LocationSearchBar component */}
+                <div className="mx-auto my-auto flex w-4/5 rounded-md bg-white flex-wrap">
+                    <div className="flex flex-1 flex-col">
+                        <div className="p-4">
+                            <HatCard
+                                // maxTempCelsius={celsius.maxTempCelsius}
+                                // minTempCelsius={celsius.minTempCelsius}
+                                maxTempCelsius={0}
+                                minTempCelsius={0}
+                            />
+                        </div>
+                        <div className="flex-1 p-4">
+                            <UpperBodyCard
+                                // maxTempCelsius={celsius.maxTempCelsius}
+                                // minTempCelsius={celsius.minTempCelsius}
+                                maxTempCelsius={0}
+                                minTempCelsius={0}
+                            />
                         </div>
                     </div>
-                    <div className="p-4">
-                        <FootCard maxTempCelsius={celsius.maxTempCelsius} minTempCelsius={celsius.minTempCelsius} />
+                    <div className="flex flex-1 flex-col">
+                        <div className="flex-4  p-4">
+                            <div className="flex-1 ">
+                                <LowerBodyCard
+                                    // maxTempCelsius={celsius.maxTempCelsius}
+                                    // minTempCelsius={celsius.minTempCelsius}
+                                    maxTempCelsius={0}
+                                    minTempCelsius={0}
+                                />
+                            </div>
+                        </div>
+                        <div className="p-4">
+                            <FootCard
+                                // maxTempCelsius={celsius.maxTempCelsius}
+                                // minTempCelsius={celsius.minTempCelsius}
+                                maxTempCelsius={0}
+                                minTempCelsius={0}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </>
-}
+        </>
+    );
+};
+
 export default ClothesPage;
