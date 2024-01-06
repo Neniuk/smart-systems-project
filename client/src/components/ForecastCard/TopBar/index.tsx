217165e894d1195db1026950580605d35eeb5faa
JesseDong9527 <154630957+JesseDong9527@users.noreply.github.com>
2024-01-06 13:50:14 +0200

    topbar

--- client/src/components/ForecastCard/TopBar/index.tsx
+++ client/src/components/ForecastCard/TopBar/index.tsx
@@ -1,12 +1,15 @@
 const ForecastTopBar = ({ selectedForecast }: { selectedForecast: string }) => {
     return (
-        <div className="flex flex-row gap-56">
-            <h1 className={selectedForecast === "weekly" ? "underline" : ""}>
+        <div className="flex flex-row gap-56 items-center">
+            <h1 className="text-gray-500">
                 Weekly Forecast
             </h1>
-            <h1 className={selectedForecast === "hourly" ? "underline" : ""}>
-                Hourly Forecast
-            </h1>
+            <div className="relative text-gray-500">
+                <h1>
+                    Hourly Forecast
+                </h1>
+                <div className="absolute bottom-[-10px] left-0 right-0 h-1 bg-white rounded-full"></div>
+            </div>
         </div>
     );
 };
