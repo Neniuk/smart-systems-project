1ab7ecaa998a7c621ed18a6b2793ef2b14c19dd5
Mattias <106891644+Neniuk@users.noreply.github.com>
2024-01-07 20:43:46 +0200

    Update index.tsx

--- client/src/pages/ActivitiesPage/index.tsx
+++ client/src/pages/ActivitiesPage/index.tsx
@@ -9,13 +9,13 @@ import getWeather from "../../api/getWeather";
 import Location from "../../models/Location";
 import BackgroundImage from "../../components/BackgroundImage";
 
-type ActivitiesPage = {
+type ActivitiesPageProps = {
     weather?: object;
     forecast?: object;
     location?: Location;
 };
 
-const ActivitiesPage = (props: ActivitiesPage) => {
+const ActivitiesPage = (props: ActivitiesPageProps) => {
     return (
         <div className="from-backgroundPrimary to-backgroundSecondary min-h-screen bg-gradient-to-b">
             <div className="content text-textLight flex flex-col items-center justify-center gap-8 pt-16"></div>
